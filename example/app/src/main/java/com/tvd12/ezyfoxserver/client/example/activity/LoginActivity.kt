526d6f79625ce26c7b1958245333820ac7da6b84
Nam Anh Nguyen <namanh11611@gmail.com>
2023-08-07 06:52:33 +0700

    Refactor LoginActivity (#16)
    * Refactor LoginActivity
    
    * Rename Extensions file

--- example/app/src/main/java/com/tvd12/ezyfoxserver/client/example/activity/LoginActivity.kt
+++ example/app/src/main/java/com/tvd12/ezyfoxserver/client/example/activity/LoginActivity.kt
@@ -2,21 +2,19 @@ package com.tvd12.ezyfoxserver.client.example.activity
 
 import android.app.Activity
 import android.content.Intent
-import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
-import androidx.annotation.StringRes
-import androidx.appcompat.app.AppCompatActivity
-import android.text.Editable
-import android.text.TextWatcher
 import android.view.View
 import android.view.inputmethod.EditorInfo
 import android.widget.Button
 import android.widget.EditText
 import android.widget.ProgressBar
 import android.widget.Toast
+import androidx.annotation.StringRes
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import com.tvd12.ezyfoxserver.client.example.R
-
+import com.tvd12.ezyfoxserver.client.example.extension.afterTextChanged
 import com.tvd12.ezyfoxserver.client.example.socket.SocketClientProxy
 import com.tvd12.ezyfoxserver.client.example.view.LoggedInUserView
 import com.tvd12.ezyfoxserver.client.example.view.LoginViewModel
@@ -24,35 +22,43 @@ import com.tvd12.ezyfoxserver.client.example.view.LoginViewModelFactory
 
 class LoginActivity : AppCompatActivity() {
 
-    private lateinit var loginViewModel: LoginViewModel
+    private val loginViewModel: LoginViewModel by lazy {
+        ViewModelProvider(this, LoginViewModelFactory()).get(LoginViewModel::class.java)
+    }
     private lateinit var username: EditText
     private lateinit var password: EditText
     private lateinit var login: Button
     private lateinit var loading: ProgressBar
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContentView(R.layout.activity_login)
 
+        initView()
+        initObserver()
+        initAction()
+    }
+
+    private fun initView() {
         username = findViewById(R.id.username)
         password = findViewById(R.id.password)
         login = findViewById(R.id.login)
         loading = findViewById(R.id.loading)
-        val socketProxy = SocketClientProxy.getInstance()
-        loginViewModel = ViewModelProvider(this, LoginViewModelFactory())
-            .get(LoginViewModel::class.java)
+    }
 
+    private fun initObserver() {
         loginViewModel.loginFormState.observe(this@LoginActivity, Observer {
             val loginState = it ?: return@Observer
 
             // disable login button unless both username / password is valid
             login.isEnabled = loginState.isDataValid
 
-            if (loginState.usernameError != null) {
-                username.error = getString(loginState.usernameError)
+            loginState.usernameError?.let { errorId ->
+                username.error = getString(errorId)
             }
-            if (loginState.passwordError != null) {
-                password.error = getString(loginState.passwordError)
+            loginState.passwordError?.let { errorId ->
+                password.error = getString(errorId)
             }
         })
 
@@ -60,15 +66,17 @@ class LoginActivity : AppCompatActivity() {
             val loginResult = it ?: return@Observer
 
             loading.visibility = View.GONE
-            if (loginResult.error != null) {
-                showLoginFailed(loginResult.error)
+            loginResult.error?.let { error ->
+                showLoginFailed(error)
             }
-            if (loginResult.success != null) {
-                updateUiWithUser(loginResult.success)
+            loginResult.success?.let { success ->
+                updateUiWithUser(success)
             }
             setResult(Activity.RESULT_OK)
         })
+    }
 
+    private fun initAction() {
         username.afterTextChanged {
             loginViewModel.loginDataChanged(
                 username.text.toString(),
@@ -96,7 +104,7 @@ class LoginActivity : AppCompatActivity() {
             }
         }
 
-        socketProxy.apply {
+        SocketClientProxy.getInstance().apply {
             onDisconnectedCallback {
                 loading.visibility = View.GONE
                 showToast("reconnecting ...")
@@ -129,25 +137,10 @@ class LoginActivity : AppCompatActivity() {
     }
 
     private fun showToast(text: String) {
-        Toast.makeText(applicationContext, text, Toast.LENGTH_LONG).show()
+        Toast.makeText(this, text, Toast.LENGTH_LONG).show()
     }
 
     private fun showLoginFailed(@StringRes errorString: Int) {
-        Toast.makeText(applicationContext, errorString, Toast.LENGTH_SHORT).show()
+        Toast.makeText(this, errorString, Toast.LENGTH_SHORT).show()
     }
-}
-
-/**
- * Extension function to simplify setting an afterTextChanged action to EditText components.
- */
-fun EditText.afterTextChanged(afterTextChanged: (String) -> Unit) {
-    this.addTextChangedListener(object : TextWatcher {
-        override fun afterTextChanged(editable: Editable?) {
-            afterTextChanged.invoke(editable.toString())
-        }
-
-        override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {}
-
-        override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {}
-    })
 }
